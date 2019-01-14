c13761dcc2d965765b70587fb0684cacd7704baa
amog-oliveira <amandam.oliveirag@gmail.com>
2019-01-14 17:29:18 -0200

    second commit

--- conversor.py
+++ conversor.py
@@ -1,47 +1,80 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-unidade = {'0': 'zero', '1': 'um', '2': 'dois', '3': 'três', '4': 'quatro', '5': 'cinco', '6': 'seis', '7': 'sete', '8': 'oito', '9': 'nove'}
-dez = {'10': 'dez', '11': 'onze', '12': 'doze', '13': 'treze', '14': 'catorze', '15': 'quinze', '16': 'dezesseis', '17': 'dezessete', '18': 'dezoito', '19': 'dezenove'}
-dezena = {'2': 'vinte', '3': 'trinta', '4': 'quarenta', '5': 'cinquenta', '6': 'sessenta', '7': 'setenta', '8': 'oitenta', '9': 'noventa'}
-milhar = {'3': 'cem', '4': 'mil', '5': 'milhão(ões)', '6': 'bilhão(ões)', '7': 'trilhão(ões)'}
+import sys
 
-def pegar_valor():
-    valor = input('Digite um preço (ex.:1,00):')
-    try:
-        reais, centavos = valor.split(',')
-    except BaseException as e:
-        print('O valor inserido não está correto')
-        raise(e)   
+extenso = [
+  {1:"um", 2:"dois", 3:"três", 4:"quatro", 5:"cinco", 6:"seis", 7:"sete", 8:"oito", 9:"nove", 10:"dez", 11:"onze", 12:"doze",
+  13:"treze", 14:"quatorze", 15:"quinze", 16:"dezesseis", 17:"dezessete", 18:"dezoito", 19:"dezenove"},
+  {2:"vinte", 3:"trinta", 4:"quarenta", 5:"cinquenta", 6:"sessenta", 7:"setenta", 8:"oitenta", 9:"noventa"},
+  {1:"cento", 2:"duzentos", 3:"trezentos", 4:"quatrocentos", 5:"quinhentos", 6:"seissentos", 7:"setessentos", 
+  8:"oitocentos", 9:"novecentos"}
+]
 
-    return (reais,centavos)
+unidade = ['', ' mil', (' milhão', ' milhões'), (' bilhão', ' bilhões'), (' trilhão', ' trilhões')]
 
-def pegar_centavos(centavos):
-    if centavos[0] != '0':
-        if centavos in dez:
-            centavos = f'{dez[centavos]} centavo(s)'
+def centenas(valor, posicao):
+    valor = '0' * (3 - len(valor)) + valor 
+
+    if valor == '000':
+        return ''
+    if valor == '100':
+        return 'cem'
+    array_extenso = ''
+    dezena = valor[1] + valor[2] 
+    if valor[0] != '0':
+        array_extenso += extenso[2][int(valor[0])]
+        if dezena != '00':
+            array_extenso +=  ' e '
         else:
-            centavos = f'{dezena[centavos[0]]} e {unidade[centavos[1]]} centavo(s)'
+            return array_extenso + (type(unidade[posicao]) == type(()) and (int(valor) > 1 and unidade[posicao][1] or unidade[posicao][0]) or unidade[posicao])
+    if int(dezena) < 20:
+        array_extenso += extenso[0][int(dezena)]
     else:
-        centavos = f'{unidade[centavos[1]]} centavo(s)'         
+        if valor[1] != '0':
+            array_extenso += extenso[1][int(valor[1])]
+        if valor[2] != '0':
+            array_extenso += ' e ' + extenso[0][int(valor[2])]
+    return array_extenso + (type(unidade[posicao]) == type(()) and (int(valor) > 1 and unidade[posicao][0]) or unidade[posicao])
 
-    return centavos
+def extenso_reais(reais_valor, centavos_valor):
+    array_extenso = []
+    posicao = 0
 
-def pegar_dezena(digitos):
-    if digitos in unidade:
-        digitos = f'{unidade[digitos]} real (is)'
-    elif digitos in dez:
-        digitos = f'{dez[digitos]} real (is)'
+    if(int(centavos_valor) == 0):
+        array_extenso.append('zero centavos')
+    elif(int(centavos_valor) == 1):
+        array_extenso.append('um centavo')
     else:
-        digitos = f'{dezena[digitos[0]]} e {unidade[digitos[1]]} real(is)'
-        
-    return digitos
-
-def pegar_unity(valor):
-    return milhar[len(valor)]
+        array_extenso.append(centenas(centavos_valor,0) + ' centavos')
+    
+    if(int(reais_valor) == 0):
+        array_extenso.append('zero reais')
+        array_extenso.reverse()
+        return ' e '.join([resultado for resultado in array_extenso if resultado])
+    elif(int(reais_valor) == 1):
+        array_extenso.append('um real')
+        array_extenso.reverse()
+        return ' e '.join([resultado for resultado in array_extenso if resultado])
+    while reais_valor:
+        valor_em_reais = reais_valor[-3:]
+        reais_valor = reais_valor[:-3]
+        if (posicao == 0):
+            array_extenso.append(centenas(valor_em_reais, posicao) + ' reais')
+        else:
+            array_extenso.append(centenas(valor_em_reais, posicao))
+        posicao += 1
+    array_extenso.reverse()
+    return ' e '.join([resultado for resultado in array_extenso if resultado])
 
 if __name__ == '__main__':
-    valores = pegar_valor()
-    digitos = pegar_dezena(valores[0])
-    centavos = pegar_centavos(valores[1])
-    print(f'{digitos} e {centavos}')
+  digitos = sys.argv[1]
+  try:
+    digito_reais, digito_centavos = digitos.split(',')
+  except:
+    print ('Erro ao ler o valor.')
+  if(len(digito_centavos) != 2):
+    print ('O valor está incorreto na casa dos centavos.')
+    sys.exit(1)
+  print (digitos)
+  print (extenso_reais(digito_reais,digito_centavos))
